a996b8ee7a6f03b092c472bbaa8664ff2ec6a649
Andrew Pynch <andrewpynchbusiness@gmail.com>
2020-05-19 09:32:45 -0700

    Fix torch requirement

--- setup.py
+++ setup.py
@@ -1,34 +1,42 @@
 from distutils.core import setup
 setup(
-  name = 'vision-utils',         # How you named your package folder (MyLib)
-  packages = ['vision-utils'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Collection of practical tools for working with image data',   # Give a short description about your library
-  author = 'Andrew Pynch',                   # Type in your name
-  author_email = 'andrewpynchbusiness@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/andrew-pynch',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['image', 'data collection', 'data processing', 'data pipeline'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'pyautogui',
-          'melee',
-          'opencv-python',
-          'pandas',
-          'matplotlib',
-          'numpy',
-          'pytorch',
-          'pillow'
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
+    name='scalary',         # How you named your package folder (MyLib)
+    packages=['scalary'],   # Chose the same as "name"
+    version='0.1.1',      # Start with a small number and increase it with every change you make
+    # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+    license='MIT',
+    # Give a short description about your library
+    description='Collection of practical tools for working with image data',
+    author='Andrew Pynch',                   # Type in your name
+    author_email='andrewpynchbusiness@gmail.com',      # Type in your E-Mail
+    # Provide either the link to your github or to your website
+    url='https://github.com/andrew-pynch',
+    # I explain this later on
+    download_url='https://github.com/Andrew-Pynch/scalary/archive/0.1.tar.gz',
+    keywords=['image', 'data collection', 'data processing',
+              'data pipeline'],   # Keywords that define your package best
+    install_requires=[            # I get to this in a second
+        'pyautogui',
+        'melee',
+        'opencv-python',
+        'pandas',
+        'matplotlib',
+        'numpy',
+        'torch',
+        'pillow'
+    ],
+    classifiers=[
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Development Status :: 3 - Alpha',
+        # Define that your audience are developers
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',   # Again, pick a license
+        # Specify which pyhton versions that you want to support
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-  ],
+    ],
 )
